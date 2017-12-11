973769f8f522c464f27edaa11e5484c6ad496bf2
Jared Chen <471238650@qq.com>
2017-12-11 20:32:38 +0800

    add HistoryService and AuthorRecommendService
    add HistoryService
    add HistoryServiceImpl
    add AuthorRecommendService
    add HistoryController

---
+++ src/main/java/cn/zju/springboot/service/HistoryService.java
@@ -0,0 +1,19 @@
+package cn.zju.springboot.service;
+
+import java.util.List;
+
+import cn.zju.springboot.pojo.History;
+
+public interface HistoryService {
+	
+	public List<History> getHistoryByUserId(int userId);
+	
+	public History getHistoryByHistoryId(int historyId);
+	
+	public void insertHistory(History history);
+	
+	public int deleteHistoryByHistoryId(int historyId);
+	
+	public int updateHistory(History history);
+
+}
