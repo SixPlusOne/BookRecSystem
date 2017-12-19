db8f604b51ab671a537ea35a496d2b57e009b6d8
andy <wangyanchao_ace@126.com>
2017-12-19 19:45:04 +0800

    add function getBooksByTagName and getBooksByTagNameList

--- src/main/java/cn/zju/springboot/service/TagService.java
+++ src/main/java/cn/zju/springboot/service/TagService.java
@@ -1,5 +1,9 @@
 package cn.zju.springboot.service;
 
+import java.util.List;
+import java.util.Set;
+
+import cn.zju.springboot.pojo.Book;
 import cn.zju.springboot.pojo.Tag;
 
 public interface TagService {
@@ -11,4 +15,10 @@ public interface TagService {
 	public int deleteTagByName(String name);
 
 	public int updateTag(String oldName, String newName);
+	
+	// 根据单个tag获得书籍
+	public Set<Book> getBooksByTagName(String name);
+	
+	// 根据多个tag获得书籍
+	public Set<Book> getBooksByTagNameList(List<String> names);
 }
