64478797eadb76d79434444fa79065538ce3a230
akiyamamio11 <akiyamamio11@126.com>
2017-12-26 13:36:07 +0800

    huang first_page tag_page

--- src/main/java/cn/zju/springboot/controller/RecommendController.java
+++ src/main/java/cn/zju/springboot/controller/RecommendController.java
@@ -4,8 +4,11 @@ import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,9 +17,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.github.abel533.entity.Example;
 
 import cn.zju.springboot.mapper.BookMapper;
+import cn.zju.springboot.pojo.Author;
 import cn.zju.springboot.pojo.Book;
 import cn.zju.springboot.pojo.Tag;
+import cn.zju.springboot.pojo.User;
+import cn.zju.springboot.service.AuthorRecommendService;
 import cn.zju.springboot.service.BookRecommendServiceImpl;
+import cn.zju.springboot.service.BookService;
 import cn.zju.springboot.service.TagServiceImpl;
 
 @Controller
@@ -26,10 +33,16 @@ public class RecommendController {
 	BookMapper bookMapper;
 	
 	@Autowired
+	BookService bookService;
+	
+	@Autowired
 	BookRecommendServiceImpl bookRecommendService;
 	
 	@Autowired
 	TagServiceImpl tagService;
+	
+	@Autowired
+	AuthorRecommendService authorRecommendService;
 	
 	@GetMapping("/recommend/{userId}")  //根据用户给出item-cf推荐书籍
 	@ResponseBody
@@ -45,10 +58,13 @@ public class RecommendController {
 	}
 	
 	@GetMapping("/tag/{tag}")   //获得指定某一标签的书籍
-	@ResponseBody
-	public List<Book> getBooksByTag(@PathVariable String tag){
-		return tagService.getBooksByTag(tag);
-		
+	public String getBooksByTag(@PathVariable String tag,Model model){
+		List<Book> books= tagService.getBooksByTag(tag);
+		int count=books.size();
+		model.addAttribute("books",books);
+		model.addAttribute("count",count);
+		model.addAttribute("tag",tag);
+		return "rank";
 	}
 	
 	@GetMapping("/test")   //测试标签存入user_tag表
@@ -63,12 +79,27 @@ public class RecommendController {
 	
 
 	@GetMapping("/tag/test")//测试根据多个标签推荐书籍
-	@ResponseBody
-	public List<Book> getBooksByMultiplyTags(){
+	public String getBooksByMultiplyTags(Model model){
 		List<String> tags=new LinkedList<String>();
 		tags.add("三国");
 		tags.add("火凤燎原");
-		return tagService.getBooksByMultiplyTags(tags);
+		List<Book> books=tagService.getBooksByMultiplyTags(tags);
+		model.addAttribute("books",books);
+		return "first_page";
+	}
+	
+	@GetMapping("/first_page")
+	public String getFirst_Page(Model model,HttpSession session) throws IOException{
+		User user=(User)session.getAttribute("CURRENT_USER");
+		List<Book> books=bookRecommendService.getRecommendBooks(123);
+		List<Author> authors=authorRecommendService.getAuthorRecommend(123);
+		model.addAttribute("books",books);
+		model.addAttribute("authors",authors);
+		List<Book> hotBooks=bookService.getHotBookList();
+		model.addAttribute("hotBooks",hotBooks);
+		return "first_page";
+		
+		
 	}
 
 }
