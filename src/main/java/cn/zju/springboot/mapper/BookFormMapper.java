6d8a58961134ac46700621036fa7c4ce5d1ea35c
akiyamamio11 <akiyamamio11@126.com>
2018-01-02 16:06:13 +0800

    rank bookform

--- src/main/java/cn/zju/springboot/mapper/BookFormMapper.java
+++ src/main/java/cn/zju/springboot/mapper/BookFormMapper.java
@@ -1,5 +1,7 @@
 package cn.zju.springboot.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Param;
 
 import com.github.abel533.mapper.Mapper;
@@ -10,5 +12,6 @@ import cn.zju.springboot.pojo.BookForm;
 public interface BookFormMapper extends Mapper<BookForm>{
 	
 	BookForm findone(@Param("id") int id);
+	List<BookForm> findBooksByTagPaged(@Param("tag") String tag);
 
 }
